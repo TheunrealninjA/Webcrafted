b9655e38298b673683037d453b622a0869e35ef2
Wyatt <wyatthardy508@gmail.com>
2024-07-20 09:55:17 +0100

    changes

---
+++ JavaScript/random.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    let el = document.getElementById('pricebox');
+    el.style.setProperty('--rand', Math.random() * 100 + 'px');
+
+});
--- Templates.php
+++ Templates.php
@@ -4,8 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Templates - WebCrafted Pro</title>
+    <meta name="description"
+        content="Welcome to WebCrafted.Pro, where you can get hand-crafted custom websites for your businesses specific needs. We have competive prices and exclusive features that elevate your website to the next level.">
+
+    <link rel="icon" href="images/WCLogo.webp">
+
+    <link rel="stylesheet" href="CSS/all.css">
+    <link rel="stylesheet" href="CSS/Animate.css">
+    <link rel="stylesheet" href="CSS/Footer.css">
+    <link rel="stylesheet" href="CSS/Nav.css">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <script src="JavaScript/AnimationWait.js"></script>
+    <style>
+        @import url("https://fonts.googleapis.com/css?family=Poppins");
+    </style>
 </head>
 <body>
-    
+    <div class="main-body Gup">
+        <header class="snap">
+            <ul class="Top-Buttons">
+                <li id="templates"><a href="Templates.php">Templates</a></li>
+                <li id="services" class="Second-Layer"><a href="Pricing.html">Pricing</a></li>
+                <li class="First-Layer"><a href="index.html">Home</a></li>
+                <li id="websites" class="Second-Layer"><a href="Websites.html">Websites</a></li>
+                <li id="contact"><a href="ContactUs.html">Contact Us</a></li>
+            </ul>
+            <a class="MiniWCLogo" href="index.html"><img src="images/MiniWCLogo.webp" alt="Logo"></a>
+        </header>
+
+        <div class="templates">
+            
+        </div>
+    </div>
 </body>
 </html>
