ca5334a2706037043e1abcf74bb6ddd450c6f3dd
Wyatt <wyatthardy508@gmail.com>
2024-07-22 10:43:22 +0100

    changes to index

--- JavaScript/random.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-    let el = document.getElementById('pricebox');
-    el.style.setProperty('--rand', Math.random() * 100 + 'px');
-
-});
