059c7be32a2953e0a36a4bb8695e282c090015a6
Wyatt <wyatthardy508@gmail.com>
2024-08-04 21:33:20 +0100

    major changes

---
+++ JavaScript/SlideShow.js
@@ -0,0 +1,19 @@
+let slideIndex = 1;
+document.addEventListener("DOMContentLoaded", function() {
+    showSlides(slideIndex);
+});
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "flex";
+}
