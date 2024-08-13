cff3139f062c596e77f3d0d9f3fcda49d29995d1
Wyatt <wyatthardy508@gmail.com>
2024-08-13 10:45:57 +0100

    :(

--- recaptcha.php
+++ recaptcha.php
@@ -1,38 +1,30 @@
 <?php
-  
-// Checking valid form is submitted or not
-if (isset($_POST['submit'])) {
-    
-    // Storing name in $name variable
-    $name = $_POST['name'];
-  
-    // Storing google recaptcha response
-    // in $recaptcha variable
-    $recaptcha = $_POST['g-recaptcha-response'];
+// Storing google recaptcha response
+// in $recaptcha variable
+$recaptcha = $_POST['g-recaptcha-response'];
 
-    // Put secret key here, which we get
-    // from google console
-    $secret_key = '6LefsiUqAAAAAKWekDhAa-FdPlv6n0iu1DIIXuTz';
+// Put secret key here, which we get
+// from google console
+$secret_key = '6LefsiUqAAAAAKWekDhAa-FdPlv6n0iu1DIIXuTz';
 
-    // Hitting request to the URL, Google will
-    // respond with success or error scenario
-    $url = 'https://www.google.com/recaptcha/api/siteverify?secret='
-          . $secret_key . '&response=' . $recaptcha;
+// Hitting request to the URL, Google will
+// respond with success or error scenario
+$url = 'https://www.google.com/recaptcha/api/siteverify?secret='
+    . $secret_key . '&response=' . $recaptcha;
 
-    // Making request to verify captcha
-    $response = file_get_contents($url);
+// Making request to verify captcha
+$response = file_get_contents($url);
 
-    // Response return by google is in
-    // JSON format, so we have to parse
-    // that json
-    $response = json_decode($response);
+// Response return by google is in
+// JSON format, so we have to parse
+// that json
+$response = json_decode($response);
 
-    // Checking, if response is true or not
-    if ($response->success == true) {
-        echo 'Google reCAPTACHA verified';
-    } else {
-        echo 'Error in Google reCAPTACHA';
-    }
+// Checking, if response is true or not
+if ($response->success == true) {
+    echo 'Google reCAPTACHA verified';
+} else {
+    echo 'Error in Google reCAPTACHA';
 }
 
 ?>
