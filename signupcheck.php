45240297a17ab5439ab9a3eb588488b18e68d2a4
Wyatt <wyatthardy508@gmail.com>
2024-08-29 21:53:09 +0100

    rewrite signup

--- signupcheck.php
+++ signupcheck.php
@@ -2,17 +2,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-session_start();
-include('dbconfig.php');
-
 if (isset($_POST['submit_button'])) {
-    $username = mysqli_real_escape_string($con, $_POST['username']);
-    $email = mysqli_real_escape_string($con, $_POST['email']);
     $password = $_POST['password'];
     $confirm_password = $_POST['confirm_password'];
 
     if ($password == $confirm_password) {
         $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+        $mysqli = require( __DIR__ ."/dbconfig.php");
         // Check Email
 
         $checkemail = "SELECT email FROM users WHERE email='$email' LIMIT 1";
@@ -22,10 +19,19 @@ if (isset($_POST['submit_button'])) {
             header("Location: Status.php?page=signup&status=emailused");
             exit(0);
         } else {
-            $stmt = $con->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+            $sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
 
             // Bind the parameters and execute the query
-            $stmt->bind_param("sss", $username, $email, $hashed_password);
+            $stmt = $mysqli->stmt_init();
+
+            $stmt->prepare($sql);
+
+            if ( ! $stmt->prepare($sql)) {
+                die("SQL Error: ". $mysqli->error);
+            }
+
+            $stmt->bind_param("sss", $_POST["username"], $_POST["email"], $hashed_password);
+
             if ($stmt->execute()) {
                 header("Location: Status.php?page=signup&status=success");
             } else {
