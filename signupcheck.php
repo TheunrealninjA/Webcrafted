95e4902c5e6576a8b936b96a5dceb578031442bf
Wyatt <wyatthardy508@gmail.com>
2024-08-30 17:55:53 +0100

    m

--- signupcheck.php
+++ signupcheck.php
@@ -11,7 +11,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    echo"Connection failed: " . $conn->connect_error;
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -40,4 +40,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $stmt->close();
     $conn->close();
+}else{
+    echo"No Post";
 }
