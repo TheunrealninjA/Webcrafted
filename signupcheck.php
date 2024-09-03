c8d679cbd60d1d371203c6eb679ad914a4581ec0
Wyatt <wyatthardy508@gmail.com>
2024-09-03 22:14:26 +0100

    a

--- signupcheck.php
+++ signupcheck.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once '/recaptcha-master/src/autoload.php';
+require_once __DIR__ . '/recaptcha-master/src/autoload.php';
 function redirectWithStatus($status)
 {
     header("Location: Status.php?page=signup&status=$status");
