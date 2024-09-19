3c103c661390a8791dba87122ba0a0432a11515a
Wyatt <wyatthardy508@gmail.com>
2024-09-19 18:40:59 +0100

    Fix include path for session_manager.php in Admin/index.php

--- Admin/index.php
+++ Admin/index.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-include(__DIR__ . 'PHPScripts/session_manager.php');
+include(__DIR__ . '/PHPScripts/session_manager.php');
 
 $is_logged_in = isset($_SESSION['username']);
 $user = htmlspecialchars($_SESSION['username']);
