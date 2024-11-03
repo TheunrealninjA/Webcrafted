55b471532ddb4000da8b68a4dc9a7d9a73144965
Wyatt <wyatthardy508@gmail.com>
2024-11-03 00:56:12 +0000

    Change error display type in database connection failure message

--- Email_Verification/index.php
+++ Email_Verification/index.php
@@ -90,7 +90,7 @@
         $conn = new mysqli($servername, $username, $password, $dbname);
 
         if ($conn->connect_error) {
-            displayMessage("error", "Error.webp", "Connection failed: " . $conn->connect_error);
+            displayMessage("errorbox", "Error.webp", "Connection failed: " . $conn->connect_error);
         }
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
